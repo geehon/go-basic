3948d36d8ac721dc5c57d2e07f9a7c48b77d8033
GeeHon <liaoliaoliao70@gmail.com>
2022-02-02 11:13:06 +0800

    add go-starter mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/geehon/go-basic
 
 go 1.15
+
+require github.com/geehon/go-starter v0.0.1
--- main/main.go
+++ main/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/geehon/go-basic/variable/typeint"
 	"github.com/geehon/go-basic/variable/typepointer"
 	"github.com/geehon/go-basic/variable/typestring"
+	"github.com/geehon/go-starter/structdemo"
 )
 
 func learnVariable() {
@@ -39,4 +40,5 @@ func main() {
 	helpfunc.DeferDemo()
 	createLine("builtin string function")
 	helpfunc.StringBuiltin()
+	structdemo.StructDedmo()
 }
