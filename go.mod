bf1f37e2432b7d8cc446b5f4bd8ba17bd3ad08bd
GeeHon <liaoliaoliao70@gmail.com>
2022-04-01 10:24:44 +0800

    handel httoserver err

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/geehon/go-basic
 
-go 1.15
+go 1.18
--- httpserver/server.go
+++ httpserver/server.go
@@ -10,17 +10,17 @@ import (
 func GetUrl() {
 	r, err := http.Get("https://example.com/api/v1/server")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
 		}
 	}(r.Body)
 	bs, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 	fmt.Printf("body=%s\n", bs)
 }
